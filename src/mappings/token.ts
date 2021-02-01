bcbc1e72070e8df12d2d8e3776a08ae1cf11a902
Raul Glogovetan <raul@glogovetan.com>
2021-02-01 21:34:56 +0200

    Remove: network event handlers

--- src/mappings/token.ts
+++ src/mappings/token.ts
@@ -2,16 +2,8 @@ import { Address, BigInt } from '@graphprotocol/graph-ts'
 
 import {
   Token as TokenContract,
-  Mint,
-  Burn,
-  LogSetAuthority,
-  LogSetOwner,
-  Approval,
-  Transfer,
 } from '../../generated/templates/Token/Token'
 import { Token as TokenSchema } from '../../generated/schema'
-
-import { handleEvent } from './event'
 
 export function createToken(tokenAddress: string): void {
   let token = TokenSchema.load(tokenAddress)
@@ -31,28 +23,4 @@ export function createToken(tokenAddress: string): void {
     }
     token.save()
   }
-}
-
-export function handleMint(event: Mint): void {
-  handleEvent("Mint(address,uint256)", event, event.address)
-}
-
-export function handleBurn(event: Burn): void {
-  handleEvent("Burn(address,uint256)", event, event.address)
-}
-
-export function handleLogSetAuthority(event: LogSetAuthority): void {
-  handleEvent("LogSetAuthority(address)", event, event.address)
-}
-
-export function handleLogSetOwner(event: LogSetOwner): void {
-  handleEvent("LogSetOwner(address)", event, event.address)
-}
-
-export function handleApproval(event: Approval): void {
-  handleEvent("Approval(address,address,uint256)", event, event.address)
-}
-
-export function handleTransfer(event: Transfer): void {
-  handleEvent("Transfer(address,address,uint256)", event, event.address)
 }
