9d73234d16bb7314c127aa52fe38410c66651b19
Raul Glogovetan <raul@glogovetan.com>
2021-04-15 13:49:35 +0300

    Add: `Motion` entity calculate required stake

--- src/mappings/votingReputation.ts
+++ src/mappings/votingReputation.ts
@@ -25,16 +25,18 @@ export function handleMotionCreated(event: MotionCreated): void {
 
   let motionId = event.params.motionId;
   let chainMotion = extension.getMotion(motionId);
+  let totalStakeFraction = extension.getTotalStakeFraction();
 
   let motion = new Motion(colony.toHexString() + "_motion_" + extension._address.toHexString() + '_' + motionId.toString());
   motion.fundamentalChainId = motionId
+  motion.action = chainMotion.action.toHexString()
   motion.associatedColony = colony.toHexString()
   motion.extensionAddress = extension._address.toHexString()
   motion.transaction = event.transaction.hash.toHexString()
   motion.agent = event.params.creator.toHexString()
   motion.domain = colony.toHexString() + '_domain_' + event.params.domainId.toString()
   motion.currentStake = chainMotion.stakes.pop()
-  motion.requiredStake = new BigInt(0)
+  motion.requiredStake = chainMotion.skillRep.times(totalStakeFraction).div(BigInt.fromI32(10).pow(18))
   motion.escalated = chainMotion.escalated
 
   motion.save()
