95d53fe1d7d678ea3147bd0ff787fea107fd6466
ArmandoGraterol <armandogrtrl@gmail.com>
2021-08-24 15:54:10 -0300

    Added WIP logic for CoinMachine block handler

--- src/mappings/coinMachine.ts
+++ src/mappings/coinMachine.ts
@@ -1,8 +1,11 @@
+import { Address, ByteArray, crypto, ethereum } from '@graphprotocol/graph-ts'
+import { ColonyExtension } from '../../generated/schema';
+
 import {
   ExtensionInitialised,
   TokensBought,
   PeriodUpdated,
-  CoinMachine as CoinMachineContract
+  CoinMachine as CoinMachineContract,
 } from '../../generated/templates/CoinMachine/CoinMachine'
 
 import { handleEvent } from './event'
@@ -26,4 +29,18 @@ export function handlePeriodUpdated(event: PeriodUpdated): void {
   let colony = extension.getColony();
 
   handleEvent("PeriodUpdated(uint256,uint256)", event, colony)
+}
+
+export function handleBlock(block: ethereum.Block): void {
+    const COIN_MACHINE = crypto.keccak256(ByteArray.fromUTF8("CoinMachine")).toHexString()
+    const { address: coinMachineAddress } = ColonyExtension.load(COIN_MACHINE);
+    const coinMachineExtension = CoinMachineContract.bind((coinMachineAddress as unknown) as Address);
+    console.log(coinMachineExtension)
+    const periodLength = coinMachineExtension.getPeriodLength();
+    const windowSize = coinMachineExtension.getWindowSize();
+    const targetPerPeriod = coinMachineExtension.getTargetPerPeriod();
+    const blockTime = block.timestamp;
+
+    // let period = new CoinMachinePeriod(colonyAddress + "_coinMachine_" + coinMachineExtension.address + '_' + timestamp.toString());
+    console.log(periodLength, windowSize, targetPerPeriod, blockTime);
 }
