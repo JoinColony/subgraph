38cce3d54120dfc17014f10070be893abd663822
Raul Glogovetan <raul@glogovetan.com>
2021-10-07 22:51:11 +0300

    Add: handle token "Transfer" events

--- src/mappings/token.ts
+++ src/mappings/token.ts
@@ -3,7 +3,19 @@ import { Address, BigInt } from '@graphprotocol/graph-ts'
 import {
   Token as TokenContract,
 } from '../../generated/templates/Token/Token'
-import { Token as TokenSchema } from '../../generated/schema'
+import {
+  Token as TokenSchema,
+  ColonyExtension as ColonyExtensionSchema,
+} from '../../generated/schema'
+import {
+  Token as TokenTemplate
+} from '../../generated/templates'
+
+import {
+  Transfer
+} from '../../generated/templates/Token/Token'
+
+import { handleEvent } from './event'
 
 export function createToken(tokenAddress: string): void {
   let token = TokenSchema.load(tokenAddress)
@@ -22,5 +34,13 @@ export function createToken(tokenAddress: string): void {
       token.symbol = symbol.value
     }
     token.save()
+    TokenTemplate.create(Address.fromString(tokenAddress))
+  }
+}
+
+export function handleTransfer(event: Transfer): void {
+  let extension = ColonyExtensionSchema.load(event.params.dst.toHexString());
+  if (extension) {
+    handleEvent("Transfer(address,address,uint256)", event, event.params.dst)
   }
 }
