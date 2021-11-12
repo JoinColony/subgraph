9304e8f3ce8309d2b6f0b04c24392a8a0bb91266
Raul Glogovetan <raul@glogovetan.com>
2021-11-12 20:57:39 +0200

    Add: `KYCAddresses` entity

--- src/mappings/whitelist.ts
+++ src/mappings/whitelist.ts
@@ -4,6 +4,8 @@ import {
   UserApproved as UserApprovedEvent,
 } from '../../generated/templates/Whitelist/Whitelist'
 
+import { KYCAddress } from '../../generated/schema'
+
 import { handleEvent } from './event'
 
 export function handleExtensionInitialised(event: ExtensionInitialised): void {
@@ -16,6 +18,17 @@ export function handleExtensionInitialised(event: ExtensionInitialised): void {
 export function handleUserApproved(event: UserApprovedEvent): void {
   let extension = WhitelistContract.bind(event.address);
   let colony = extension.getColony();
+
+  let kycAddress = KYCAddress.load(event.params._user.toHexString())
+  if (kycAddress == null) {
+    kycAddress = new KYCAddress(event.params._user.toHexString())
+
+    kycAddress.extension = event.address.toHexString()
+  }
+
+  kycAddress.status = event.params._status
+
+  kycAddress.save()
 
   handleEvent("UserApproved(address,bool)", event, colony)
 }
