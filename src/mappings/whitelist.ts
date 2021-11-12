d6a815d4d4d01e1dafaf33b2b6bee40aa049ac2e
Raul Glogovetan <raul@glogovetan.com>
2021-11-12 17:01:24 +0200

    Add: handle whitelist `UserApproved` event

--- src/mappings/whitelist.ts
+++ src/mappings/whitelist.ts
@@ -1,6 +1,7 @@
 import {
   ExtensionInitialised,
   Whitelist as WhitelistContract,
+  UserApproved as UserApprovedEvent,
 } from '../../generated/templates/Whitelist/Whitelist'
 
 import { handleEvent } from './event'
@@ -10,4 +11,11 @@ export function handleExtensionInitialised(event: ExtensionInitialised): void {
   let colony = extension.getColony();
 
   handleEvent("ExtensionInitialised()", event, colony)
+}
+
+export function handleUserApproved(event: UserApprovedEvent): void {
+  let extension = WhitelistContract.bind(event.address);
+  let colony = extension.getColony();
+
+  handleEvent("UserApproved(address,bool)", event, colony)
 }
