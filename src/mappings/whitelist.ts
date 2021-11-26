f663c7787fe82796dd4d5150f9b1b09096d271e1
Raul Glogovetan <raul@glogovetan.com>
2021-11-26 13:17:52 +0200

    Remove: unused `KYCAddress` entity

--- src/mappings/whitelist.ts
+++ src/mappings/whitelist.ts
@@ -5,8 +5,6 @@ import {
   AgreementSigned as AgreementSignedEvent,
 } from '../../generated/templates/Whitelist/Whitelist'
 
-import { KYCAddress } from '../../generated/schema'
-
 import { handleEvent } from './event'
 
 export function handleExtensionInitialised(event: ExtensionInitialised): void {
@@ -19,35 +17,12 @@ export function handleExtensionInitialised(event: ExtensionInitialised): void {
 export function handleUserApproved(event: UserApprovedEvent): void {
   let extension = WhitelistContract.bind(event.address);
   let colony = extension.getColony();
-
-  let kycAddress = KYCAddress.load(event.params._user.toHexString())
-  if (kycAddress == null) {
-    kycAddress = new KYCAddress(event.params._user.toHexString())
-
-    kycAddress.extension = event.address.toHexString()
-  }
-
-  kycAddress.status = event.params._status
-
-  kycAddress.save()
-
   handleEvent("UserApproved(address,bool)", event, colony)
 }
 
 export function handleAgreementSigned(event: AgreementSignedEvent): void {
   let extension = WhitelistContract.bind(event.address);
   let colony = extension.getColony();
-
-  let kycAddress = KYCAddress.load(event.params._user.toHexString())
-  if (kycAddress == null) {
-    kycAddress = new KYCAddress(event.params._user.toHexString())
-
-    kycAddress.extension = event.address.toHexString()
-  }
-
-  kycAddress.status = true
-
-  kycAddress.save()
 
   handleEvent("AgreementSigned(address)", event, colony)
 }
