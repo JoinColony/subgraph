b0df0aa47f5e69a9e747c5b2de355c9df24d0fef
Raul Glogovetan <raul@glogovetan.com>
2022-05-02 21:28:07 +0300

    Update: additional_abis script now tracks Coin Machine

--- scripts/append_abis.js
+++ scripts/append_abis.js
@@ -2,34 +2,69 @@ const path = require('path');
 const { writeFileSync } = require("fs");
 
 const ICOLONY_PATH = process.env.CUSTOM_ICOLONY_PATH ? path.resolve(__dirname, process.env.CUSTOM_ICOLONY_PATH) : path.resolve(__dirname, '..', '..', 'colonyNetwork/build/contracts/IColony.json');
-const ADDITIONAL_ABIS_PATH = path.resolve(__dirname, './additionalAbis.json');
+const COINMACHINE_PATH = process.env.CUSTOM_COINMACHINE_PATH ? path.resolve(__dirname, process.env.CUSTOM_COINMACHINE_PATH) : path.resolve(__dirname, '..', '..', 'colonyNetwork/build/contracts/CoinMachine.json');
 
-console.log('interfaces path')
-console.log(ICOLONY_PATH)
+const ADDITIONAL_COLONY_ABIS_PATH = path.resolve(__dirname, './additionalColonyAbis.json');
+const ADDITIONAL_COINMACHINE_ABIS_PATH = path.resolve(__dirname, './additionalCoinMachineAbis.json');
 
+console.log('interfaces path(s)')
+console.log('Colony:', ICOLONY_PATH)
+console.log('Coin Machine:', COINMACHINE_PATH)
+
+/*
+ * Colony
+ */
 const IColony = require(ICOLONY_PATH);
-const additionalAbis = require(ADDITIONAL_ABIS_PATH);
+const additionalColonyAbis = require(ADDITIONAL_COLONY_ABIS_PATH);
+const neededColonyAbis = [];
+const existingColonySigs = IColony.abi.map(({ name, inputs }) => `${name}(${inputs.map((parameter) => parameter.type).join(",")})`);
 
-const neededAbis = [];
-
-const existingSigs = IColony.abi.map(({ name, inputs }) => `${name}(${inputs.map((parameter) => parameter.type).join(",")})`);
-
-additionalAbis.map((abiEntry) => {
+additionalColonyAbis.map((abiEntry) => {
   const { name, inputs } = abiEntry;
   const signature = `${name}(${inputs.map((parameter) => parameter.type).join(",")})`;
-  if (existingSigs.indexOf(signature) === -1) {
-    neededAbis.push(abiEntry);
+  if (existingColonySigs.indexOf(signature) === -1) {
+    neededColonyAbis.push(abiEntry);
   }
 });
 
-console.log('Adding following abis')
-console.log(neededAbis)
+console.log('Adding following Colony abis')
+console.log(neededColonyAbis)
 
 writeFileSync(ICOLONY_PATH, JSON.stringify({
   ...IColony,
   abi: [
     ...IColony.abi,
-    ...neededAbis,
+    ...neededColonyAbis,
+  ],
+}), {
+  encoding: "utf8",
+});
+
+
+/*
+ * Coin Machine
+ */
+const CoinMachine = require(COINMACHINE_PATH);
+const additionalCoinMachineAbis = require(ADDITIONAL_COINMACHINE_ABIS_PATH);
+const neededCoinMachineAbis = [];
+const existingCoinMachineSigs = CoinMachine.abi.map(({ name, inputs }) => `${name}(${inputs.map((parameter) => parameter.type).join(",")})`);
+
+additionalCoinMachineAbis.map((abiEntry) => {
+  const { name, inputs } = abiEntry;
+  const signature = `${name}(${inputs.map((parameter) => parameter.type).join(",")})`;
+  if (existingCoinMachineSigs.indexOf(signature) === -1) {
+    neededCoinMachineAbis.push(abiEntry);
+  }
+});
+
+console.log('Adding following Coin Machine abis')
+console.log(neededCoinMachineAbis)
+
+writeFileSync(COINMACHINE_PATH, JSON.stringify({
+  ...CoinMachine,
+  abi: [
+    ...CoinMachine.abi,
+    ...neededCoinMachineAbis,
   ],
 }), {
   encoding: "utf8",
