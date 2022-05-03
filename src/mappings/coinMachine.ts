6176decffe38241242944fd8d2c5f11b68198919
Raul Glogovetan <raul@glogovetan.com>
2022-05-03 22:21:08 +0300

    Add: handle both new and historic `TokensBought` events

--- src/mappings/coinMachine.ts
+++ src/mappings/coinMachine.ts
@@ -4,6 +4,7 @@ import { CoinMachinePeriod } from '../../generated/schema';
 import {
   ExtensionInitialised,
   TokensBought,
+  TokensBought1 as HistoricTokensBought,
   PeriodUpdated,
   CoinMachine as CoinMachineContract,
 } from '../../generated/templates/CoinMachine/CoinMachine'
@@ -17,7 +18,7 @@ export function handleExtensionInitialised(event: ExtensionInitialised): void {
   handleEvent("ExtensionInitialised()", event, colony)
 }
 
-export function handleTokensBought(event: TokensBought): void {
+export function handleHistoricTokensBought(event: HistoricTokensBought): void {
   let extension = CoinMachineContract.bind(event.address);
   let colony = extension.getColony();
 
@@ -51,6 +52,42 @@ export function handleTokensBought(event: TokensBought): void {
   }
 
   handleEvent("TokensBought(address,uint256,uint256)", event, colony)
+}
+
+export function handleHTokensBought(event: TokensBought): void {
+  let extension = CoinMachineContract.bind(event.address);
+  let colony = extension.getColony();
+
+  let numTokens = event.params.numTokens;
+  let periodLength = extension.getPeriodLength();
+  let periodPrice = extension.getCurrentPrice();
+
+  let blockTime = event.block.timestamp;
+
+  // Get the timestamp of when the current period is supposed to end and convert it to milliseconds
+  // blockTime.minus(blockTime.mod(periodLength)) when did the period started
+  // .plus(periodLength) when it's going to end
+  let salePeriodEnd = blockTime.minus(blockTime.mod(periodLength)).plus(periodLength).times(BigInt.fromI32(1000));
+
+  let periodId = colony.toHexString() + "_coinMachine_" + extension._address.toHexString() + '_' + salePeriodEnd.toString();
+  let coinMachinePeriod = CoinMachinePeriod.load(periodId);
+
+  if (coinMachinePeriod != null) {
+    coinMachinePeriod.tokensBought = coinMachinePeriod.tokensBought.plus(numTokens);
+    coinMachinePeriod.price = periodPrice;
+
+    coinMachinePeriod.save();
+  } else {
+    coinMachinePeriod = new CoinMachinePeriod(periodId)
+    coinMachinePeriod.saleEndedAt = salePeriodEnd;
+    coinMachinePeriod.colonyAddress = colony.toHexString();
+    coinMachinePeriod.tokensBought = BigInt.fromI32(0).plus(numTokens);
+    coinMachinePeriod.price = periodPrice;
+
+    coinMachinePeriod.save();
+  }
+
+  handleEvent("TokensBought(address,address,uint256,uint256)", event, colony)
 }
 
 export function handlePeriodUpdated(event: PeriodUpdated): void {
